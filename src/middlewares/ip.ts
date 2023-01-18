bc50def8804d43bc4bcc7479b8f5d419467ef5b3
Jordan Fokoua <jfokoua@gmail.com>
2023-01-18 17:21:01 +0100

    feat(Core): IP Middelwares
    - Set ip in clientIp req prop  using middleware

--- src/middlewares/ip.ts
+++ src/middlewares/ip.ts
@@ -1,6 +1,6 @@
 import { getClientIp } from '@supercharge/request-ip';
 
 export const ipMiddleware = (req, res, next) => {
-  req.ip = getClientIp(req);
+  req.clientIp = getClientIp(req);
   next();
 };
